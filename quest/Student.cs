a2c8f2a2f8c83430c8cf30a2357fb3af86824f0e
DdTt11 <112966069+DdTt11@users.noreply.github.com>
2022-12-05 01:18:00 +0700

    fixed Student class

--- quest/Student.cs
+++ quest/Student.cs
@@ -1,90 +1,38 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace UniExamQuest
 {
     class Student
     {
-        private int health;
-        private bool study;
-        private int money;
-        private int mind;
-        private int happiness;
+        public int Health { get; set; }
+        public bool IsStudy { get; set; }
+        public int Money { get; set; }
+        public int Mind { get; set; }
+        public int Happiness { get; set; }
+        public bool IsSick { get; set; }
+        public int StipendSize { get; set; }
         public Student()
         {
-            health = 80;
-            study = false;
-            mind = 50;
-            money = 5000;
-            happiness = 70;
+            Happiness = 100;
+            Health = 100;
+            Mind = 100;
+            StipendSize = 0;
+            Money = 0;
+            IsStudy = false;
+            IsSick = false;
         }
-        public void Get_sick()
+        public void FallSick()
         {
-            health -= 30;
-            happiness -= 10;
+            IsSick = true;
         }
-        public void Get_well()
+        public void Recover()
         {
-            health += 25;
-            happiness += 15;
+            IsSick = false;
         }
-        public void Get_money_from_work()
+        public void GetStipend()
         {
-            money += 10000;
-            happiness += 20;
+            if (IsStudy)
+            {
+                Money += StipendSize;  
+            }
         }
-        public void Get_stipend()
-        {
-            money += 5000;
-            happiness += 30;
-        }
-        public void Get_money_from_parents()
-        {
-            money += 7000;
-            happiness += 30;
-        }
-        public void buy_food()
-        {
-            money -= 2000;
-            happiness += 5;
-        }
-        public void go_to_club()
-        {
-            money -= 1500;
-            health -= 10;
-            happiness += 50;
-            mind -= 5;
-        }
-        public void buy_medicine()
-        {
-            money -= 2000;
-            health += 10;
-
-        }
-        public void prepare_to_exam()
-        {
-            mind += 30;
-            health -= 3;
-            happiness -= 10;
-        }
-        public void go_to_work()
-        {
-            happiness -= 10;
-            health -= 10;
-            mind += 5;
-        }
-        public void take_part_in_concerts()
-        {
-            happiness += 40;
-
-        }
-        public void go_to_lecture()
-        {
-            happiness -= 40;
-            health -= 5;
-            mind += 20;
-        }
-
     }
 }
