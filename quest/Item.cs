e0604018c830952563a9939942793bbc21b5d873
DdTt11 <112966069+DdTt11@users.noreply.github.com>
2022-12-05 02:59:14 +0700

    added Store and Item classes

---
+++ quest/Item.cs
@@ -0,0 +1,20 @@
+﻿namespace UniExamQuest
+{
+    class Item
+    {
+        public Properties? Prop;
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public Item(double price, string name)
+        {
+            Price = price;
+            Name = name;
+            Prop = new Properties();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Price}";
+        }
+    }
+}
---
+++ quest/Store.cs
@@ -0,0 +1,25 @@
+﻿namespace UniExamQuest
+{
+    class Store
+    {
+        public List<Item> Items;
+        
+        public Store()
+        {
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            if (Items != null)
+            {
+                Items.Add(item);
+            }
+        }
+
+        public List<Item> GetItems()
+        {
+            return Items.OrderBy(i => i.Price).ToList();
+        }
+    }
+}
