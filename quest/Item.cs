8d645d91388f8e4a42fd341adf71d7fee95fe58f
jeorji <118723195+jeorji@users.noreply.github.com>
2022-12-09 10:47:31 +0700

    implemented IDeserializable interface for Item

--- quest/Item.cs
+++ quest/Item.cs
@@ -1,6 +1,8 @@
-﻿namespace UniExamQuest
+﻿using System.Xml;
+
+namespace UniExamQuest
 {
-    class Item : Activity
+    class Item : Activity, IDeserializable<Item>
     {
         public double Price { get; set; }
 
@@ -9,9 +11,46 @@
             Price = price;
         }
 
+        // test
+        public Item() : base("") { }
+
         public override string ToString()
         {
             return $"{Name}: {Price}";
+        }
+
+        public Item Deserialize(string xmlString)
+        {
+            XmlDocument itemsDoc = new XmlDocument();
+
+            var iProp = typeof(Item);
+            Item resultItem = new Item();
+
+            // test
+            itemsDoc.LoadXml("<root>" + xmlString + "</root>");
+            XmlElement? rootElement = itemsDoc.DocumentElement;
+
+            if (rootElement is null)
+                throw new NotImplementedException();
+
+            foreach (XmlElement node in rootElement)
+            {
+                var prop = iProp.GetProperty(node.Name);
+                if (prop is null)
+                    throw new NotImplementedException();
+
+                var propType = prop.PropertyType;
+
+                if (propType == typeof(double))
+                {
+                    double doubleValue;
+                    double.TryParse(node.InnerText, out doubleValue);
+                    prop.SetValue(resultItem, doubleValue, null);
+                }
+                else
+                    prop.SetValue(resultItem, node.InnerText, null);
+            }
+            return resultItem;
         }
     }
 }
