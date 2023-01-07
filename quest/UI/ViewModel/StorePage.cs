ffe12e5be0b548475073656ba37299a1f25a4c9e
jeorji <118723195+jeorji@users.noreply.github.com>
2023-01-07 19:11:37 +0700

    added StorePage

---
+++ quest/UI/ViewModel/StorePage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using UI.Model;
+
+namespace UI.ViewModel
+{
+    internal class StorePage
+    {
+        public ObservableCollection<UniExamQuest.Item> AvailableItems { get; set; }
+        public UniExamQuest.Item SelectedItem { get; set; }
+        public StorePage() 
+        {
+            var items = MODEL.GM.State.Storage.Items;
+            AvailableItems = new ObservableCollection<UniExamQuest.Item>(items);
+        }
+        private Command? _buyItem;
+        public Command BuyItem
+        {
+            get
+            {
+                _buyItem ??= new Command(
+                    p => isMoneyEnought(),
+                    p =>
+                    {
+                        MODEL.GM.State.Player.BuyItem(SelectedItem);
+                        MessageBox.Show($"Куплено {SelectedItem.Name}!");
+                    });
+                return _buyItem;
+            }
+        }
+        private bool isMoneyEnought()
+        {
+            if (SelectedItem is not null)
+                return MODEL.GM.State.Player.Money >= SelectedItem.Price;
+            return false;
+        }
+    }
+}
